16390d5dcab9531844961057a5deaf91f75ce64d
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2025-06-30 00:39:47 +0200

    Added mockup that uses SoulsFormats

---
+++ src/translators/fromscript2gd/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SoulsFormats;
+namespace fromscript2gd;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ESD talk = ESD.Read(@"~/Documents/Bloodborne/chr/c0000.esd.dcx");
+    }
+}
